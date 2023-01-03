9694ef2d181b343aa1fd028db5249dd9274577f1
“ofugeez” <eofugara@gamil.com>
2023-01-02 23:39:42 -0800

    String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,104 +1,67 @@
 #!/usr/bin/python3
-"""""This module create a rectangle"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """
-    class that generate a rectangle
-    """
-    def __init__(self, width=0, height=0):
-        """Constructor initialize the class rectangle
+    """Represent a rectangle."""
 
-        Keyword Arguments:
-           width {int} -- width of the rectangle (default: {0})
-           height {int} -- height of the rectangle (default: {0})
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Getter function of width
-
-        Returns:
-            int -- width fo the rectangle
-        """
+        """Get/set the width of the Rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter function to width
-
-        Arguments:
-            value {int} -- [valeu of width]
-            
-        Raises:
-            TypeError: width must be an integer
-            ValueError: width must be >= 0
-        """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-
         self.__width = value
 
     @property
     def height(self):
-        """Getter function to height
-        
-        returns:
-            int -- height of the rectangle
-            """
-            return self.__height
+        """Get/set the height of the Rectangle."""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter function os height
-
-        Arguments:
-            value {int} -- Value of height
-            
-        Raises:
-            TypeError: height must be an integer
-            ValueError: height must be >= 0
-        """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-
         self.__height = value
 
-        def area(self):
-            """Generate area of the rectangle
+    def area(self):
+        """Return the area of the Rectangle."""
+        return (self.__width * self.__height)
 
-            Returns:
-                Int -- total area of rectangle
-            """
-            return self.__width * self.__height
+    def perimeter(self):
+        """Return the perimeter of the Rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
 
-        def perimeter(self):
-            """Find the perimeter of a rectangle
-            
-            Returns:
-                int -- total perimeter of the rectangle
-            """
-            if self.__width == 0 or self.__height == 0:
-                return 0
-            return 2 * (self.__height + self.__width)
+    def __str__(self):
+        """Return the printable representation of the Rectangle.
+        
+        Represents the rectangle with the # character.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ("")
 
-        def __str__(self):
-            """Method that get string representation rectangle
-
-            Returns:
-                string -- string representation
-            """
-            string = ""
-            if self.__width == 0 or self.__height == 0:
-                return ""
-            for row in range(self.__height):
-                for col in range(self.__width):
-                    string = string + '#'
-                string = string + '\n'
-            return string[:-1]
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.___height - 1:
+                rect.append("\n")
+        return ("".join(rect))
