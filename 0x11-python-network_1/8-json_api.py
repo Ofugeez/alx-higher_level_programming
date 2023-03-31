586f22c3aa5a951e6c8864e00b19d522f910cfd5
“ofugeez” <eofugara@gamil.com>
2023-03-31 08:34:44 -0700

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
+"""Takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
 """
-Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
-"""
+
 import sys
 import requests
 
-
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5000/search_user"
-    if len(sys.argv) > 1:
-        q = sys.argv[1]
+    if len(sys.argv) == 1:
+        letter = ""
     else:
-        q = ""
-    payload = {"q": q}
-    response = requests.post(url, data=payload)
+        letter = sys.argv[1]
+
+    payload = {'q': letter}
+
+    request = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+
     try:
-        json_outp = response.json()
-        if not json_outp:
-            print("No result")
+        response = request.json()
+        if response == {}:
+            print('No result')
         else:
-            my_id = json_outp.get("id")
-            my_name = json_outp.get("name")
-            print("[{}] {}".format(my_id, my_name))
-    except ValueError as invalid_json:
-        print("Not a valid JSON")
+            print('[{}] {}'.format(response.get('id'), response.get('name')))
+    except ValueError:
+        print('Not a valid JSON')
